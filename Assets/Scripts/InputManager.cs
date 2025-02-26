641adb57afef4c947c3c4951e6d17e17966ffb2e
Domenic Cannella <dac36@pct.edu>
2025-02-26 15:15:18 -0500

    ORB!!!

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -12,6 +12,7 @@ public class InputManager : MonoBehaviour
     public InputField userInput; // the input field object
     public Text inputText; // part of the input field where user enters response
     public Text placeHolderText; // part of the input field for initial placeholder text
+    public Button abutton;
     
     private string story; // holds the story to display
     private List<string> commands = new List<string>(); //valid user commands
@@ -30,21 +31,57 @@ public class InputManager : MonoBehaviour
     {
         commands.Add("go");
         commands.Add("get");
-        userInput.onEndEdit.AddListener(UpdateStory);
+
+        userInput.onEndEdit.AddListener(GetInput); //now calls GetInput
+        //abutton.onClick.AddListener(DoSomething);
         story = storyText.text;
     }
 
-    public void UpdateStory(string msg)
+    //void DoSomething() //event handler
+    //{
+        //Debug.Log("Button clicked!");
+    //}
+
+    public void UpdateStory(string msg) //update display
+    {
+         story += "\n" + msg;
+         storyText.text = story;
+    }
+
+    void GetInput(string msg) //process input
     {
         if (msg != "")
         {
             char[] splitInfo = { ' ' };
-            string [] parts = msg.ToLower().Split(splitInfo); //['go', 'north']
+            string[] parts = msg.ToLower().Split(splitInfo); //['go', 'north']
 
             if (commands.Contains(parts[0])) //if valid command
             {
-                story += "\n" + msg;
-                storyText.text = story;
+                if (parts[0] == "go") //wants to switch rooms
+                {
+                    if (NavigationManager.instance.SwitchRooms(parts[1])) //returns true if direction exits
+                    {
+                        //fill in later
+                    }
+                    else
+                    {
+                        UpdateStory("Exit does not exist. Try again.");
+                    }
+                }
+                else if (parts[0] == "get")
+                {
+                    if (NavigationManager.instance.TakeItem(parts[1]))
+                    {
+                        GameManager.instance.inventory.Add(parts[1]);
+                        UpdateStory("You added a(n) " + parts[1] + " to your inventory.");
+                    }
+                    else
+                    {
+                        UpdateStory("Sorry, " + parts[1] + " does not exist in this room.");
+                    }
+
+                }
+                //UpdateStory(msg);
             }
 
         }
---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class NavigationManager : MonoBehaviour
+{
+    public static NavigationManager instance;
+    public Room startingRoom;
+    public Room currentRoom;
+    public Exit toKeyNorth;
+
+    private Dictionary<string, Room> exitRooms = new Dictionary<string, Room>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Debug.Log(startingRoom.description);
+        toKeyNorth.isHidden = true;
+        currentRoom = startingRoom;
+        Unpack();
+    }
+
+   void Unpack()
+    {
+        string description = currentRoom.description;
+        exitRooms.Clear();
+        foreach(Exit e in currentRoom.exits)
+        {
+            if (!e.isHidden)
+            {
+                description += " " + e.description;
+                exitRooms.Add(e.direction.ToString(), e.room);
+            }
+        }
+
+        InputManager.instance.UpdateStory(description);
+    }
+
+    public bool SwitchRooms(string direction)
+    {
+        if(exitRooms.ContainsKey(direction)) //if that exit exists
+        {
+            currentRoom = exitRooms[direction];
+            InputManager.instance.UpdateStory("You go " + direction);
+            Unpack();
+            return true;
+        }
+        return false;
+    }
+
+    public bool TakeItem(string item)
+    {
+        if (item == "key" && currentRoom.hasKey)
+            return true;
+        else if (item == "orb" && currentRoom.hasOrb)
+        {
+            toKeyNorth.isHidden = false;
+            return true;
+        }
+        else
+            return false;
+    }
+}
